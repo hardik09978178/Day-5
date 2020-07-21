4911e2df2cde45c2a58619454cb7304e0c1b5051
hardik09978178 <68243533+hardik09978178@users.noreply.github.com>
2020-07-21 12:35:50 +0530

    Add files via upload

---
+++ Filter.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<body>
+
+		<h1>JavaScript CompilerResponse</h1>
+
+		<script>
+			var nums = [1, 2, 3, 4, 5];
+			var oddNumbers = nums.filter(num => num % 2 !== 0 );
+			console.log(nums);
+				
+				</script>
+	</body>
+</html>
